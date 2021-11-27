27d42f6d62df04f04cfad109b833cec892a63596
Luiz Gustavo <luizgsdcr@al.insper.edu.br>
2021-11-27 13:10:10 -0300

    adicionando as imagens na saida

--- jogo.py
+++ jogo.py
@@ -34,7 +34,13 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.fill((0, 0, 0))  # Preenche com a cor azul
+    window.fill((0, 0, 0))  # Preenche com a cor branca
+    window.blit(background, (0, 0))
+    window.blit(moto_img, (0, 0))
+    window.blit(carro1_img, (150, 0))
+    window.blit(carro2_img, (300, 0))
+    window.blit(velha_img, (700, 0))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
