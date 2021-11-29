9a0dbb0b47695c82343b5d4b29e3ad8789af2a75
Fernando Kaizer <fernandock1@al.insper.edu.br>
2021-11-29 16:34:14 -0300

    adicionando classe ao jogo

--- jogo.py
+++ jogo.py
@@ -48,6 +48,32 @@ class velha(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
+
+# Classe para criação da moto
+class Moto(pygame.sprite.Sprite):
+    def __init__(self, img):
+        # Construtor da classe mãe (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image = moto_img
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, WIDTH-100)
+        self.rect.y = -100
+        self.speedx = random.randint(-3, 3)
+        self.speedy = random.randint(2, 6)
+
+    def update(self):
+        # Atualizando a posição da moto
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        # Se a moto passar do final da tela, volta para cima e sorteia
+        # novas posições e velocidades
+        if self.rect.top > HEIGHT or self.rect.right < 0 or self.rect.left > WIDTH:
+            self.rect.x = random.randint(0, WIDTH-100)
+            self.rect.y = -100
+            self.speedx = random.randint(-3, 3)
+            self.speedy = random.randint(2, 6)
+
 clock = pygame.time.Clock()
 FPS = 30
 all_sprites = pygame.sprite.Group()
@@ -65,16 +91,16 @@ while game:
         if event.type == pygame.KEYDOWN:
     # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                velha1.speedx -= 1
+                velha1.speedx -= 8
             if event.key == pygame.K_RIGHT:
-                velha1.speedx += 1
+                velha1.speedx += 8
         # Verifica se soltou alguma tecla.
         if event.type == pygame.KEYUP:
         # Dependendo da tecla, altera a velocidade.
             if event.key == pygame.K_LEFT:
-                velha1.speedx += 1
+                velha1.speedx += 8
             if event.key == pygame.K_RIGHT:
-                velha1.speedx -= 1
+                velha1.speedx -= 8
 
     # ----- Gera saídas
     all_sprites.update()
